5909bd8be01b685e132cc0e9a6e0b6e2598bc063
zhangpeng210 <zhangpeng12@jd.com>
2017-02-10 18:40:24 +0800

    test

--- src/com/tzy/test/Test.java
+++ src/com/tzy/test/Test.java
@@ -2,5 +2,16 @@ package com.tzy.test;
 
 public class Test {
 	public static void main(String[] args) {
+		int a = 10;
+		int b = 10;
+		method(a,b);
+		System.out.println("a=" + a);
+		System.out.println("b=" + b);
 	}
+	public static void  method(int a,int b){
+		System.out.println("a=100");
+		System.out.println("b=200");
+		System.exit(0);
+	}
+
 }
