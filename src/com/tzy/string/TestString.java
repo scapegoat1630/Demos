66eb876fc1f6b344057bba8a5efa49f45e85ee5e
zhangpeng210 <zhangpeng12@jd.com>
2017-02-28 12:55:58 +0800

    utf-8字符串长度

--- src/com/tzy/string/TestString.java
+++ src/com/tzy/string/TestString.java
@@ -3,9 +3,22 @@ package com.tzy.string;
 public class TestString {
 	private static final String privateString ="taobao";
 	public static void main(String[] args) {
+		test2();
+	}
+
+	private static void test1() {
 		String a = "tao";
 		String b = "bao";
 		System.out.println(a+b == privateString);
+		System.out.println(privateString.equals(a+b));
+		System.out.println(a + b == a + b);
+	}
+	private static void test2() {
+		String a = "Œ“ «–°∂π¶¡";
+		System.out.println(a.length());
+		for(int i = 0 ; i < a.length() ; i++){
+			System.out.println(a.charAt(i));
+		}
 	}
 
 }
